2c9fd483906f0c9bacc0964c16665f85da04c287
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2023-07-29 11:16:38 -0400

    restart repo

--- lib/env.dart
+++
@@ -1,3 +0,0 @@
-const supabaseURL = "https://kqtzvhtjjcitwnihlxzk.supabase.co";
-const supabaseAnonKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxdHp2aHRqamNpdHduaWhseHprIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjE3MzkzMiwiZXhwIjoyMDAxNzQ5OTMyfQ.fTx7TgCn6T01fRpXFalvF9HkgwwYtB7oCJ-Ow6SO14E";
