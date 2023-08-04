f8998532353cdb05902fa3e3808bfd80795991dc
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2023-08-03 21:49:59 -0400

    adding dashboard

---
+++ lib/features/Dashboard/presentation/Dashboard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('this a test')),
+      bottomNavigationBar:
+          BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.abc), label: "tab1"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "tab2")
+      ]),
+    );
+  }
+}
