0206d7383d891e7948ccec3f6ea1f4267cb3f505
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2023-08-04 21:39:17 -0400

    adding bottomtab

--- lib/features/Dashboard/presentation/Dashboard.dart
+++ lib/features/Dashboard/presentation/Dashboard.dart
@@ -1,22 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutterplayground/features/Leaderboard/presentation/LeaderBoard.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
-
   @override
   State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _currentTab = 0;
+
+  static const List<Widget> _tabWidgets = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
+
+  void handleTabSelection(int tab) => setState(() {
+        _currentTab = tab;
+      });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('this a test')),
+      body: Center(child: _tabWidgets.elementAt(_currentTab)),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.abc), label: "tab1"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "tab2"),
+          BottomNavigationBarItem(icon: Icon(Icons.abc), label: "Games"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), label: "LeaderBoards"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Settings"),
         ],
+        onTap: handleTabSelection,
       ),
     );
   }
