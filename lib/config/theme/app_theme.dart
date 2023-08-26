25a2427d0add0b68300796af51df35e5426bbdbb
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2023-08-26 10:29:17 -0400

    developed tabbar

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 
 const Color _primaryColor = Colors.cyan;
+const MaterialColor _primarySwatch = Colors.cyan;
 
 final ThemeData appTheme = ThemeData(
   primaryColor: _primaryColor,
+  primarySwatch: _primarySwatch,
 );
+
+
+
+// ThemeData(
+//         // Define the default brightness and colors.
+//         brightness: Brightness.dark,
+//         primaryColor: Colors.lightBlue[800],
+
+//         // Define the default font family.
+//         fontFamily: 'Georgia',
+
+//         // Define the default `TextTheme`. Use this to specify the default
+//         // text styling for headlines, titles, bodies of text, and more.
+//         textTheme: const TextTheme(
+//           displayLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+//           titleLarge: TextStyle(fontSize: 36, fontStyle: FontStyle.italic),
+//           bodyMedium: TextStyle(fontSize: 14, fontFamily: 'Hind'),
+//         ),
+//       ),
--- lib/features/Dashboard/presentation/widget/custom_navigation_bar.dart
+++ lib/features/Dashboard/presentation/widget/custom_navigation_bar.dart
@@ -28,31 +28,22 @@ class CustomNavigationBar extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.all(20),
       decoration: const BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.all(
-          Radius.circular(40),
-        ),
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(Radius.circular(40)),
       ),
       child: DefaultTabController(
         initialIndex: currentTab,
         length: numberOfTabs,
         animationDuration: const Duration(),
         child: TabBar(
-          indicator: const BoxDecoration(color: Colors.red),
+          indicator: const BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+          ),
           tabs: navigationBarItems,
           onTap: onTabSelect,
         ),
       ),
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return BottomNavigationBar(
-  //     backgroundColor: Colors.amber,
-  //     items: navigationBarItems,
-  //     onTap: onTabSelect,
-  //     currentIndex: currentTab,
-  //   );
-  // }
 }
