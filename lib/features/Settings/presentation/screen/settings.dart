29c16626609e62a70d586e192e4176b79577b7a5
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2023-08-31 12:12:28 -0400

    adding center to settings screen

--- lib/features/Settings/presentation/screen/settings.dart
+++ lib/features/Settings/presentation/screen/settings.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -12,15 +10,17 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('settings'),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('toggle theme'),
-        )
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('settings'),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('toggle theme'),
+          )
+        ],
+      ),
     );
   }
 }
